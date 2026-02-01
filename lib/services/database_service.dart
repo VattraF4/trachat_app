2e9089f73994132e9823f77056f268dccd80b65d
VattraF4 <vattraramo13@gmail.com>
2026-02-01 23:01:00 +0700

    add function to query snap shot message and users

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -7,7 +7,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 const String USER_COLLECTION = "Users";
 const String CHAT_COLLECTION = "Chats";
-const String MESSAGE_COLLECTION = "Messages";
+const String MESSAGE_COLLECTION = "messages";
 
 class DatabaseService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -34,6 +34,23 @@ class DatabaseService {
 
   Future<DocumentSnapshot> getUser(String uid) {
     return _db.collection(USER_COLLECTION).doc(uid).get();
+  }
+
+  Stream<QuerySnapshot> getChatsForUsers(String uid) {
+    return _db
+        .collection(CHAT_COLLECTION)
+        .where("members", arrayContains: uid)
+        .snapshots();
+  }
+
+  Future<QuerySnapshot> getLastMessageForChat(String chatID) {
+    return _db
+        .collection(CHAT_COLLECTION)
+        .doc(chatID)
+        .collection(MESSAGE_COLLECTION)
+        .orderBy("send_time", descending: true)
+        .limit(1)
+        .get();
   }
 
   Future<void> updateUserLastSeenTime(String uid) async {
